f049ac5f4ab2dd14f41b7d0ee21618c15466d3cb
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2024-10-30 11:54:02 -0400

    Broken three-d version

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,25 @@
-use std::ops::RangeInclusive;
-
-use egui::containers;
 use three_d::*;
-use lens_rs::*;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     pollster::block_on(run());
 }
 
-fn reset_button<'a, Path: Copy + 'a, S: Lens<Path, impl PartialEq> + Default>(
-    settings: &'a mut S,
-    path: Path,
-) -> impl FnOnce(&mut egui::Ui) -> egui::Response + 'a {
+fn settings_panel<'a>(
+    inspector_visible: &'a mut bool,
+) -> impl FnMut(&mut egui::Ui) -> () + 'a {
     move |ui| {
-        let mut reset_response = ui.add_enabled(
-            *settings.view_mut(path) != S::default().view(path),
-            egui::Button::new("⟲"),
-        );
-        if reset_response.clicked() {
-            *settings.view_mut(path) = S::default().view(path);
-            reset_response.mark_changed();
+        let response = ui.horizontal(|ui| {
+            ui.label("This label has the tooltip, but does not show it when hovered over");
+        }).response;
+
+        if response.contains_pointer() {
+            response.show_tooltip_text("Test");
         }
-        reset_response
-    }
-}
 
-fn number_setting<'a, Path: Copy + 'a, F: egui::emath::Numeric + PartialEq>(
-    name: &'a str,
-    settings: &'a mut (impl Lens<Path, F> + Default),
-    path: Path,
-    range: RangeInclusive<F>,
-) -> impl FnOnce(&mut egui::Ui) -> egui::Response + 'a {
-    move |ui| {
-        let mut changed_response = ui.horizontal(|ui| {
-            ui.add(reset_button(settings, path)).changed()
-                || ui
-                    .add(egui::Slider::new(settings.view_mut(path), range).text(name))
-                    .changed()
-        });
-        if changed_response.inner {
-            changed_response.response.mark_changed();
-        }
-        changed_response.response
-    }
-}
+        ui.checkbox(inspector_visible, "Show Inpsector");
 
-#[derive(Debug, Lens)]
-struct Settings {
-    #[optic]
-    slider_val: f32,
-}
-
-impl Default for Settings {
-    fn default() -> Self {
-        Self { slider_val: 0.0 }
+        ui.horizontal(|ui| ui.label("Hovering over this label shows the tooltip"));
     }
 }
 
@@ -69,39 +34,41 @@ async fn run() {
     .unwrap();
     let context = window.gl();
 
+    let mut inspector_visible = false;
+
     let mut gui = GUI::new(&context);
 
-    // Our application state:
-    let mut inspector_visible = false;
-    let mut settings = Settings::default();
+    // Start the main render loop
+    window.render_loop(
+        move |mut frame_input| // Begin a new frame with an updated frame input
+        {
+            let mut panel_width = 0.0;
+            gui.update(
+                &mut frame_input.events,
+                frame_input.accumulated_time,
+                frame_input.viewport,
+                frame_input.device_pixel_ratio,
+                |gui_context| {
+                    use three_d::egui::*;
+                    let panel_response = CentralPanel::default().show(gui_context, settings_panel(&mut inspector_visible));
+                    panel_width = panel_response.response.rect.width();
 
-    window.render_loop(move |mut frame_input| {
-        gui.update(&mut frame_input.events, frame_input.accumulated_time, frame_input.viewport, frame_input.device_pixel_ratio, |ctx| {
-            egui::SidePanel::left("Test").show(ctx, |ui| {
-                ui.heading("My egui Application");
-                let result = ui.add(number_setting("Broken setting", &mut settings, optics!(slider_val), 0.0..=5.0));
-                if result.contains_pointer() {
-                    containers::show_tooltip_for(&ctx, result.id, &result.rect, |ui| ui.label("Test"));
-                }
-                ui.collapsing("This triggers the bug", |_ui| {});
-                // ui.checkbox(&mut inspector_visible, "Show inspector");
-                // ui.label("However, the hover tooltip only shows up when over this label");
-            });
+                egui::Window::new("🔍 Inspection")
+                    .open(&mut inspector_visible)
+                    .vscroll(true)
+                    .show(gui_context, |ui| {
+                        gui_context.inspection_ui(ui);
+                    });
+                },
+            );
 
-            egui::Window::new("🔍 Inspection")
-                .open(&mut inspector_visible)
-                .vscroll(true)
-                .show(ctx, |ui| {
-                    ctx.inspection_ui(ui);
-                });
-        });
+            // Get the screen render target to be able to render something on the screen
+            let screen = frame_input.screen();
 
-        // Get the screen render target to be able to render something on the screen
-        let screen = frame_input.screen();
+            // Render the GUI to the screen render target
+            screen.write(|| gui.render()).unwrap();
 
-        // Render the GUI to the screen render target
-        screen.write(|| gui.render()).unwrap();
-
-        FrameOutput::default()
-    });
+            FrameOutput::default()
+        }
+    );
 }
