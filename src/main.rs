b7c4083ad3baf6643376b2f6a51fc3a08017f2a7
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2024-10-30 09:42:43 -0400

    Working three-d & lens-rs version

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,60 @@
-use std::{num, ops::RangeInclusive};
+use std::ops::RangeInclusive;
 
 use egui::containers;
 use three_d::*;
+use lens_rs::*;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     pollster::block_on(run());
 }
 
-fn reset_button<'a>(
-    setting: &'a mut f32,
+fn reset_button<'a, Path: Copy + 'a, S: Lens<Path, impl PartialEq> + Default>(
+    settings: &'a mut S,
+    path: Path,
 ) -> impl FnOnce(&mut egui::Ui) -> egui::Response + 'a {
     move |ui| {
         let mut reset_response = ui.add_enabled(
-            *setting != 0.0,
+            *settings.view_mut(path) != S::default().view(path),
             egui::Button::new("⟲"),
         );
         if reset_response.clicked() {
-            *setting = 0.0;
+            *settings.view_mut(path) = S::default().view(path);
             reset_response.mark_changed();
         }
         reset_response
     }
 }
 
-fn number_setting<'a>(
+fn number_setting<'a, Path: Copy + 'a, F: egui::emath::Numeric + PartialEq>(
     name: &'a str,
-    setting: &'a mut f32,
-    range: RangeInclusive<f32>,
+    settings: &'a mut (impl Lens<Path, F> + Default),
+    path: Path,
+    range: RangeInclusive<F>,
 ) -> impl FnOnce(&mut egui::Ui) -> egui::Response + 'a {
     move |ui| {
         let mut changed_response = ui.horizontal(|ui| {
-            ui.add(reset_button(setting)).changed()
+            ui.add(reset_button(settings, path)).changed()
                 || ui
-                    .add(egui::Slider::new(setting, range).text(name))
+                    .add(egui::Slider::new(settings.view_mut(path), range).text(name))
                     .changed()
         });
         if changed_response.inner {
             changed_response.response.mark_changed();
         }
         changed_response.response
+    }
+}
+
+#[derive(Debug, Lens)]
+struct Settings {
+    #[optic]
+    slider_val: f32,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self { slider_val: 0.0 }
     }
 }
 
@@ -58,17 +73,17 @@ async fn run() {
 
     // Our application state:
     let mut inspector_visible = false;
-    let mut slider_val = 0.0;
+    let mut settings = Settings::default();
 
     window.render_loop(move |mut frame_input| {
         gui.update(&mut frame_input.events, frame_input.accumulated_time, frame_input.viewport, frame_input.device_pixel_ratio, |ctx| {
-            egui::SidePanel::left("Main_left_panel").show(ctx, |ui| {
+            egui::SidePanel::left("Test").show(ctx, |ui| {
                 ui.heading("My egui Application");
-                let result = ui.add(number_setting("Broken setting", &mut slider_val, 0.0..=5.0));
+                let result = ui.add(number_setting("Broken setting", &mut settings, optics!(slider_val), 0.0..=5.0));
                 if result.contains_pointer() {
                     containers::show_tooltip_for(&ctx, result.id, &result.rect, |ui| ui.label("Test"));
                 }
-                ui.collapsing("This triggers the bug", |ui| {});
+                ui.collapsing("This triggers the bug", |_ui| {});
                 // ui.checkbox(&mut inspector_visible, "Show inspector");
                 // ui.label("However, the hover tooltip only shows up when over this label");
             });
